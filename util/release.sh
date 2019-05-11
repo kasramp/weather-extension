653f65af771aac72db61aa3188997b65356a3d4b
kasramp <kasra_mp@yahoo.com>
2019-05-11 13:42:03 +0200

    #2 do not download go in the repository folder

--- util/release.sh
+++ util/release.sh
@@ -11,10 +11,17 @@ then
 	echo "Start releasing version $TAG_VERSION ..."
 	#git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
 	#git push --tags
+	mkdir -p /tmp/go
+	cd /tmp/go
+	wget https://dl.google.com/go/go1.12.2.linux-amd64.tar.gz
+    sudo tar -xf go1.12.2.linux-amd64.tar.gz
+    sudo mv go /usr/local
+    export GOROOT=/usr/local/go
+	cd -
 	mkdir -p /tmp/weather-extension
 	yes | cp -rf * /tmp/weather-extension
 	rm -rvf /tmp/weather-extension/util
-	sudo google-chrome-stable --no-sandbox --pack-extension /tmp/weather-extension --pack-extension-key
+	sudo chromium --no-sandbox --pack-extension /tmp/weather-extension --pack-extension-key
 	ls /tmp/weather-extension
 	ls
 fi
