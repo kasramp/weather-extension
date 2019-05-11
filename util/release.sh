9252ba5c54674bd976b31d31c57c9e01e6e72181
kasramp <kasra_mp@yahoo.com>
2019-05-11 14:26:33 +0200

    #2 use default go

--- util/release.sh
+++ util/release.sh
@@ -11,14 +11,14 @@ then
 	echo "Start releasing version $TAG_VERSION ..."
 	#git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
 	#git push --tags
-	wget -P /tmp/ https://dl.google.com/go/go1.12.2.linux-amd64.tar.gz
-    sudo tar -C /tmp/ -xf /tmp/go1.12.2.linux-amd64.tar.gz
-    sudo mv /tmp/go /usr/local
-    export GOROOT=/usr/local/go
+	#wget -nv -P /tmp/ https://dl.google.com/go/go1.12.2.linux-amd64.tar.gz
+    #sudo tar -C /tmp/ -xf /tmp/go1.12.2.linux-amd64.tar.gz
+    #sudo mv /tmp/go /usr/local
+    #export GOROOT=/usr/local/go
 	mkdir -p /tmp/weather-extension
 	yes | cp -rf * /tmp/weather-extension
 	rm -rvf /tmp/weather-extension/util
-	chromium-browser --no-sandbox --pack-extension=/tmp/weather-extension/
+	google-chrome-stable --no-sandbox --pack-extension=/tmp/weather-extension/
 	ls /tmp/weather-extension
 	ls /tmp/
 	go get github.com/aktau/github-release
