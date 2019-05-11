a834a7979b851c9742aac91dbe6a505c56cfb8f5
kasramp <kasra_mp@yahoo.com>
2019-05-11 15:44:40 +0200

    #2 push tag to GitHub

--- util/release.sh
+++ util/release.sh
@@ -8,8 +8,10 @@ LAST_RELEASE=`git describe --abbrev=0` || LAST_RELEASE=v0.0
 if [ "$TAG_VERSION" != "$LAST_RELEASE" ]
 then
 	echo "Start releasing version $TAG_VERSION ..."
+	git remote rm origin && git remote add origin https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git
 	git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
 	git push --tags
+	#git push --set-upstream origin master
 	echo "Finished releasing version $TAG_VERSION!"
 	# don't publish .crx file
 	#mkdir -p /tmp/weather-extension
