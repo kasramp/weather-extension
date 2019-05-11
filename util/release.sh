0bd45c8142c2f18adaeeb0a68563509f34638b4a
kasramp <kasra_mp@yahoo.com>
2019-05-11 13:07:26 +0200

    #2 initial ci implementation

---
+++ util/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TAG_VERSION=`cat ../manifest.json | jq '. | .version' | tr -d '"'`
+TAG_VERSION=v$TAG_VERSION
+
+LAST_RELEASE=`git describe --abbrev=0` || LAST_RELEASE=v0.0
+
+
+if [ "$TAG_VERSION" != "$LAST_RELEASE" ]
+then
+	echo "Start releasing version $TAG_VERSION ..."
+	git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
+	git push --tags
+	mkdir -p /tmp/weather-extension
+	yes | cp -rf * /tmp/weather-extension
+	rm -rvf /tmp/weather-extension/util
+	sudo chrome --no-sandbox --pack-extension /tmp/weather-extension --pack-extension-key
+	ls /tmp/weather-extension
+	ls
+fi
