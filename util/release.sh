9e54afd3998f79351f7391623ab6d021585ca175
kasramp <kasra_mp@yahoo.com>
2019-05-11 13:25:45 +0200

    #2 use macOS for Chrome

--- util/release.sh
+++ util/release.sh
@@ -9,8 +9,8 @@ LAST_RELEASE=`git describe --abbrev=0` || LAST_RELEASE=v0.0
 if [ "$TAG_VERSION" != "$LAST_RELEASE" ]
 then
 	echo "Start releasing version $TAG_VERSION ..."
-	git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
-	git push --tags
+	#git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
+	#git push --tags
 	mkdir -p /tmp/weather-extension
 	yes | cp -rf * /tmp/weather-extension
 	rm -rvf /tmp/weather-extension/util
