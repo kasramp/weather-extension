28c0e5e2078766bf362fb7167dd73df69f91955d
kasramp <kasra_mp@yahoo.com>
2019-05-11 15:02:57 +0200

    #2 remove chrome and crx release

--- util/release.sh
+++ util/release.sh
@@ -8,18 +8,12 @@ LAST_RELEASE=`git describe --abbrev=0` || LAST_RELEASE=v0.0
 if [ "$TAG_VERSION" != "$LAST_RELEASE" ]
 then
 	echo "Start releasing version $TAG_VERSION ..."
-	#git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
-	#git push --tags
-	#wget -nv -P /tmp/ https://dl.google.com/go/go1.12.2.linux-amd64.tar.gz
-    #sudo tar -C /tmp/ -xf /tmp/go1.12.2.linux-amd64.tar.gz
-    #sudo mv /tmp/go /usr/local
-    #export GOROOT=/usr/local/go
-	mkdir -p /tmp/weather-extension
-	yes | cp -rf * /tmp/weather-extension
-	rm -rvf /tmp/weather-extension/util
-	google-chrome-stable --pack-extension=/tmp/weather-extension/
-	#chromium-browser --pack-extension=/tmp/weather-extension/
-	ls /tmp/
-	go get github.com/aktau/github-release
-	github-release --help
+	git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
+	git push --tags
+	echo "Finished releasing version $TAG_VERSION!"
+	# don't publish .crx file
+	#mkdir -p /tmp/weather-extension
+	#yes | cp -rf * /tmp/weather-extension
+	#rm -rvf /tmp/weather-extension/util
+	#google-chrome-stable --pack-extension=/tmp/weather-extension/
 fi
