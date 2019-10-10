520c3452a4509a1a45eb04d6612a7035948dadea
kasramp <kasra_mp@yahoo.com>
2019-10-10 16:14:57 +0200

    #7 fix more issue of the release

--- util/release.sh
+++ util/release.sh
@@ -10,7 +10,7 @@ then
 	echo "Trying to build the project ..."
 	npm install && npm run build && npm run build-zip
 	echo "Start releasing version $TAG_VERSION ..."
-	npm run release
+	npm run release --minor --ci
 	#git remote rm origin && git remote add origin https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git
 	#git tag -a $TAG_VERSION -m "Release $TAG_VERSION"
 	#git push --tags
